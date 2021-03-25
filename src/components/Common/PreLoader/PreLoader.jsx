0154f105f18f1612a58a38b6206ed2feb50a3b94
lexxA87 <alieksiei.nikonov.87@mail.ru>
2021-03-25 21:14:53 +0300

    Commit 22 "Add PreLoader for User's Page"

---
+++ src/components/Common/PreLoader/PreLoader.jsx
@@ -0,0 +1,12 @@
+import preloader from "../../../assets/images/preloader.gif";
+import React from "react";
+
+const PreLoader = (props) => {
+  return (
+    <div>
+      <img src={preloader} />
+    </div>
+  );
+};
+
+export default PreLoader;
--- src/redux/users-reducer .js
+++ src/redux/users-reducer .js
@@ -3,12 +3,14 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET-USERS";
 const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
 const SET_TOTAL_USER_COUNT = "SET_TOTAL_USER_COUNT";
+const TOGGLE_IS_FETCHING = "TOGGLE_IS_FETCHING";
 
 let initialState = {
   users: [],
   pageSize: 10,
   totalUsersCount: 0,
   currentPage: 1,
+  isFetching: true,
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -48,6 +50,11 @@ const usersReducer = (state = initialState, action) => {
         ...state,
         totalUsersCount: action.count,
       };
+    case TOGGLE_IS_FETCHING:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+      };
     default:
       return state;
   }
@@ -67,6 +74,11 @@ export const setCurrentPageAC = (currentPage) => ({
 export const setTotalUsersCountAC = (totalUsersCount) => ({
   type: SET_TOTAL_USER_COUNT,
   count: totalUsersCount,
+});
+
+export const toggleIsFetchingAC = (isFetching) => ({
+  type: TOGGLE_IS_FETCHING,
+  isFetching,
 });
 
 export default usersReducer;
