07ae53453492a9649e41efa7ab3d1e8ad327f845
lexxA87 <alieksiei.nikonov.87@mail.ru>
2021-05-06 20:51:33 +0300

    Commit 49: React.lazy и React.Suspense.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Route } from "react-router-dom";
+import {connect} from "react-redux";
+import {Route} from "react-router-dom";
 import "./App.css";
 import PreLoader from "./components/Common/PreLoader/PreLoader";
-import DiologuesContainer from "./components/Diologues/DiologuesContainer";
+//import DiologuesContainer from "./components/Diologues/DiologuesContainer";
 import Footer from "./components/Footer/Footer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
@@ -12,40 +12,45 @@ import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import Settings from "./components/Settings/Settings";
-import UsersContainer from "./components/Users/UsersContainer";
-import { initializeApp } from "./redux/app-reducer";
+//import UsersContainer from "./components/Users/UsersContainer";
+import {initializeApp} from "./redux/app-reducer";
+import {withSuspense} from "./hoc/withSuspense";
+
+const DiologuesContainer = React.lazy(() => import('./components/Diologues/DiologuesContainer'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
 
 class App extends React.Component {
-  componentDidMount() {
-    this.props.initializeApp();
-  }
-  render() {
-    if (!this.props.inilitialized) {
-      return <PreLoader />;
+    componentDidMount() {
+        this.props.initializeApp();
     }
 
-    return (
-      <div className="app-wrapper">
-        <HeaderContainer />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
-          <Route path="/messages" render={() => <DiologuesContainer />} />
-          <Route path="/news" render={() => <News />} />
-          <Route path="/music" render={() => <Music />} />
-          <Route path="/settings" render={() => <Settings />} />
-          <Route path="/users" render={() => <UsersContainer />} />
-          <Route path="/login" render={() => <LoginPage />} />
-        </div>
+    render() {
+        if (!this.props.inilitialized) {
+            return <PreLoader/>;
+        }
 
-        <Footer />
-      </div>
-    );
-  }
+        return (
+            <div className="app-wrapper">
+                <HeaderContainer/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
+                    <Route path="/messages" render={withSuspense(DiologuesContainer)}/>
+                    <Route path="/news" render={() => <News/>}/>
+                    <Route path="/music" render={() => <Music/>}/>
+                    <Route path="/settings" render={() => <Settings/>}/>
+                    <Route path="/users" render={withSuspense(UsersContainer)}/>
+                    <Route path="/login" render={() => <LoginPage/>}/>
+                </div>
+
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => ({
-  inilitialized: state.app.inilitialized,
+    inilitialized: state.app.inilitialized,
 });
 
-export default connect(mapStateToProps, { initializeApp })(App);
+export default connect(mapStateToProps, {initializeApp})(App);
