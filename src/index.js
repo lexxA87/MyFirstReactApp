cf1149c11f2196283dc681cff926551845042269
lexxA87 <alieksiei.nikonov.87@mail.ru>
2021-05-17 20:18:14 +0300

    Commit 49: HashRouter and Deploy on GitHub Page!

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/redux-store";
-import { BrowserRouter } from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import React from "react";
 import App from "./App";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <HashRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </HashRouter>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
